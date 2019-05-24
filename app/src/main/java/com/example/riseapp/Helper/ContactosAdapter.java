4b326c649d21788670e848b904a668660837ae0f
Rafa <gonzalezrafata@gmail.com>
2019-05-24 23:39:58 +0200

    cambios

--- app/src/main/java/com/example/riseapp/Helper/ContactosAdapter.java
+++ app/src/main/java/com/example/riseapp/Helper/ContactosAdapter.java
@@ -8,30 +8,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.riseapp.Contacte;
 import com.example.riseapp.Fragments.ContactaFragment;
 import com.example.riseapp.R;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class ContactosAdapter extends RecyclerView.Adapter<ContactosAdapter.ViewHolder>{
-    private final LinkedList<Integer> mContactos;
+    private final ArrayList<Contacte> mContactos;
     private LayoutInflater mInflater;
 
-    public ContactosAdapter(Context context, LinkedList<Integer> contactos){
+
+    public ContactosAdapter(Context context, ArrayList<Contacte> contactos){
         mInflater = LayoutInflater.from(context);
         mContactos = contactos;
     }
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, final int viewType) {
         View mItemView = mInflater.inflate(R.layout.basic_recycler_itemlist, parent, false);
         final ViewHolder mViewHolder = new ViewHolder(mItemView,this);
         mItemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LatLng posicion = null;
-                ContactaFragment.changeMap(posicion);
+                LatLng posicion;
+                Contacte contactoActual = mContactos.get(viewType);
+
+                    posicion = new LatLng(contactoActual.getLATITUT(), contactoActual.getLONGITUT());
+                    ContactaFragment.changeMap(posicion);
             }
         });
         return mViewHolder;
@@ -40,8 +46,8 @@ public class ContactosAdapter extends RecyclerView.Adapter<ContactosAdapter.View
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        int mCurrent = mContactos.get(position);
-        holder.contactosItemView.setText("Contacto "+Integer.toString(mCurrent));
+        Contacte mCurrent = mContactos.get(position);
+        holder.contactosItemView.setText(mCurrent.getNOM());
     }
 
     @Override
